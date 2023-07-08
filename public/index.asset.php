cecc2ad48678eb50a92ffcf1ebd789da44e6cf88
Justin Tadlock <justintadlock@gmail.com>
2023-07-08 18:25:39 -0500

    Some cleanup

--- public/index.asset.php
+++ public/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '597a5d17cb9bb8a60d9d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '5e4595aabdecad9cce5b');

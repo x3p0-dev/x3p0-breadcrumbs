9912a28f39b98dae880278c48cc538d361ff7c56
Justin Tadlock <justintadlock@gmail.com>
2023-07-09 09:58:28 -0500

    Better CSS support for PHP version

--- public/index.asset.php
+++ public/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'afcf22f4ab755a82b5b1');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'e20fa4c3a8cc16bcd45c');

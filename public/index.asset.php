17209ba5dd1a7e71eed918594a1b41a6f5989a6f
Justin Tadlock <justintadlock@gmail.com>
2023-07-09 21:05:24 -0500

    Enable link color by default
    Displays the link color in the inspector controls by default. Props @bph
    
    Fixes https://github.com/x3p0-dev/x3p0-breadcrumbs/issues/1

--- public/index.asset.php
+++ public/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'b9dca976a095636b0ba6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'aeb5de1fb4105292bd6d');

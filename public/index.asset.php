5d639925a987f2f14f9569881132b3c22390414c
Justin Tadlock <justintadlock@gmail.com>
2023-07-10 07:51:15 -0500

    Bump apiVersion to 3

--- public/index.asset.php
+++ public/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '82066353cd91e102f563');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'af2a88f0613c0ca404f3');

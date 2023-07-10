74c548105d5167520a903cc79810c7e54119dc18
Justin Tadlock <justintadlock@gmail.com>
2023-07-09 21:19:25 -0500

    Add letter-spacing support

--- public/index.asset.php
+++ public/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'aeb5de1fb4105292bd6d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '82066353cd91e102f563');

e279e50a89b7aa1cc18cf238659412757267a53f
Justin Tadlock <justintadlock@gmail.com>
2023-07-11 19:43:22 -0500

    API Version 2
    V3 won't be available until WP 6.3, and the plan is to release with support for 6.2 initially.

--- public/index.asset.php
+++ public/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '8c2c3d3572211b4162dc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '7c8d912c53a3a94921ee');

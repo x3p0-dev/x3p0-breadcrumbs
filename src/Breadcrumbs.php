97f915df55e418468062b7d25f7ded63418dae5c
Justin Tadlock <justintadlock@gmail.com>
2024-10-04 23:21:48 -0500

    Return early in Breadcrumbs.

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -168,122 +168,122 @@ class Breadcrumbs implements BreadcrumbsContract
 		// Get an array of all the available breadcrumbs from the builder.
 		$crumbs = $this->all();
 
-		if ($crumbs) {
-			// HTML allowed in labels. Everything else gets stripped out.
-			$allowed_html = [
-				'abbr'    => [ 'title' => true ],
-				'acronym' => [ 'title' => true ],
-				'code'    => true,
-				'em'      => true,
-				'strong'  => true,
-				'i'       => true,
-				'b'       => true
-			];
-
-			$count     = count($crumbs);
-			$i         = 1;
-			$show_last = $this->option('show_trail_end');
-
-			// Loop through each of the crumbs and build out a list.
-			foreach ($crumbs as $crumb) {
-				// Add `.screen-reader-text` class for crumbs
-				// with hidden labels. Usually applied to the
-				// home crumb when it's replaced with an icon.
-				$hidden = $crumb->visuallyHidden()
-					? ' screen-reader-text'
-					: '';
-
-				// Break out of the loop if this is the last item
-				// and we're not supposed to show the trail end.
-				if ($i === $count && ! $show_last) {
-					break;
-				}
-
-				// Filter out any unwanted HTML from the label.
-				$label = sprintf(
-					'<span class="%s" itemprop="name">%s</span>',
-					esc_attr($this->option('item_label_class') . $hidden),
-					wp_kses($crumb->label(), $allowed_html)
-				);
-
-				// Get the crumb URL.
-				$url = $crumb->url();
-
-				// Wrap the label with a link if the crumb has
-				// one and this isn't the last item.
-				if ($url && $i !== $count) {
-					$item = sprintf(
-						'<a href="%s" class="%s" itemprop="item">%s</a>',
-						esc_url($url),
-						esc_attr($this->option('item_content_class')),
-						$label
-					);
-				} else {
-					$item = sprintf(
-						'<span class="%s" itemscope itemid="%s" itemtype="https://schema.org/WebPage" itemprop="item">%s</span>',
-						esc_attr($this->option('item_content_class')),
-						esc_url($url),
-						$label
-					);
-				}
-
-				// Get the base class to build modifier classes from.
-				$base_class = explode(' ', $this->option('item_class'));
-				$base_class = array_shift($base_class);
-
-				$classes = [
-					$this->option('item_class'),
-					sprintf("{$base_class}--%s", $crumb->type())
-				];
-
-				// Build the list item.
-				$list .= sprintf(
-					'<%1$s class="%2$s" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">%3$s<meta itemprop="position" content="%4$s"/></%1$s>',
-					tag_escape($this->option('item_tag')),
-					esc_attr(join(' ', $classes)),
-					$item,
-					$i
-				);
-
-				++$i;
-			}
-
-			// Build the list HTML.
-			$list = sprintf(
-				'<%1$s class="%2$s" itemscope itemtype="https://schema.org/BreadcrumbList">%3$s</%1$s>',
-				tag_escape($this->option('list_tag')),
-				esc_attr($this->option('list_class')),
-				$list
-			);
-
-			// Build the title HTML only if there's a label for it.
-			if ($this->label('title')) {
-				$title = sprintf(
-					'<%1$s class="%2$s">%3$s</%1$s>',
-					tag_escape($this->option('title_tag')),
-					esc_attr($this->option('title_class')),
-					$this->label('title')
-				);
-			}
-
-			if ($this->option('container_tag')) {
-				$container = sprintf(
-					'<%1$s class="%2$s" role="navigation" aria-label="%3$s" itemprop="breadcrumb">%4$s</%1$s>',
-					tag_escape($this->option('container_tag')),
-					esc_attr($this->option('container_class')),
-					esc_attr($this->label('aria_label')),
-					'%1$s%2$s'
-				);
-			}
-
-			// Build out the final breadcrumbs trail HTML.
-			$html = sprintf($container ?: '%1$s%2$s', $title, $list);
-
-			// Add before/after wrappers.
-			$html = $this->option('before') . $html . $this->option('after');
+		if (! $crumbs) {
+			return $html;
 		}
 
-		return $html;
+		// HTML allowed in labels. Everything else gets stripped out.
+		$allowed_html = [
+			'abbr'    => [ 'title' => true ],
+			'acronym' => [ 'title' => true ],
+			'code'    => true,
+			'em'      => true,
+			'strong'  => true,
+			'i'       => true,
+			'b'       => true
+		];
+
+		$count     = count($crumbs);
+		$i         = 1;
+		$show_last = $this->option('show_trail_end');
+
+		// Loop through each of the crumbs and build out a list.
+		foreach ($crumbs as $crumb) {
+			// Add `.screen-reader-text` class for crumbs
+			// with hidden labels. Usually applied to the
+			// home crumb when it's replaced with an icon.
+			$hidden = $crumb->visuallyHidden()
+				? ' screen-reader-text'
+				: '';
+
+			// Break out of the loop if this is the last item
+			// and we're not supposed to show the trail end.
+			if ($i === $count && ! $show_last) {
+				break;
+			}
+
+			// Filter out any unwanted HTML from the label.
+			$label = sprintf(
+				'<span class="%s" itemprop="name">%s</span>',
+				esc_attr($this->option('item_label_class') . $hidden),
+				wp_kses($crumb->label(), $allowed_html)
+			);
+
+			// Get the crumb URL.
+			$url = $crumb->url();
+
+			// Wrap the label with a link if the crumb has
+			// one and this isn't the last item.
+			if ($url && $i !== $count) {
+				$item = sprintf(
+					'<a href="%s" class="%s" itemprop="item">%s</a>',
+					esc_url($url),
+					esc_attr($this->option('item_content_class')),
+					$label
+				);
+			} else {
+				$item = sprintf(
+					'<span class="%s" itemscope itemid="%s" itemtype="https://schema.org/WebPage" itemprop="item">%s</span>',
+					esc_attr($this->option('item_content_class')),
+					esc_url($url),
+					$label
+				);
+			}
+
+			// Get the base class to build modifier classes from.
+			$base_class = explode(' ', $this->option('item_class'));
+			$base_class = array_shift($base_class);
+
+			$classes = [
+				$this->option('item_class'),
+				sprintf("{$base_class}--%s", $crumb->type())
+			];
+
+			// Build the list item.
+			$list .= sprintf(
+				'<%1$s class="%2$s" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">%3$s<meta itemprop="position" content="%4$s"/></%1$s>',
+				tag_escape($this->option('item_tag')),
+				esc_attr(join(' ', $classes)),
+				$item,
+				$i
+			);
+
+			++$i;
+		}
+
+		// Build the list HTML.
+		$list = sprintf(
+			'<%1$s class="%2$s" itemscope itemtype="https://schema.org/BreadcrumbList">%3$s</%1$s>',
+			tag_escape($this->option('list_tag')),
+			esc_attr($this->option('list_class')),
+			$list
+		);
+
+		// Build the title HTML only if there's a label for it.
+		if ($this->label('title')) {
+			$title = sprintf(
+				'<%1$s class="%2$s">%3$s</%1$s>',
+				tag_escape($this->option('title_tag')),
+				esc_attr($this->option('title_class')),
+				$this->label('title')
+			);
+		}
+
+		if ($this->option('container_tag')) {
+			$container = sprintf(
+				'<%1$s class="%2$s" role="navigation" aria-label="%3$s" itemprop="breadcrumb">%4$s</%1$s>',
+				tag_escape($this->option('container_tag')),
+				esc_attr($this->option('container_class')),
+				esc_attr($this->label('aria_label')),
+				'%1$s%2$s'
+			);
+		}
+
+		// Build out the final breadcrumbs trail HTML.
+		$html = sprintf($container ?: '%1$s%2$s', $title, $list);
+
+		// Add before/after wrappers and return.
+		return $this->option('before') . $html . $this->option('after');
 	}
 
 	/**

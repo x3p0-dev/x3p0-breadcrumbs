f43ee8f2ceb698462169eb86f34b42a1be0224a0
Justin Tadlock <justintadlock@gmail.com>
2024-10-09 08:22:36 -0500

    Add an abstract `Markup` class.
    Just abstracting away some shared functionality.

--- src/Markup/Html.php
+++ src/Markup/Html.php
@@ -11,86 +11,10 @@
 
 namespace X3P0\Breadcrumbs\Markup;
 
-use X3P0\Breadcrumbs\Contracts\{Breadcrumbs, Crumb, Markup};
+use X3P0\Breadcrumbs\Contracts\Crumb;
 
-class Html implements Markup
+class Html extends Markup
 {
-	/**
-	 * @var  array
-	 * @todo Type hint with PHP 8.3+ requirement.
-	 */
-	protected const ALLOWED_HTML = [
-		'abbr'    => [ 'title' => true ],
-		'acronym' => [ 'title' => true ],
-		'code'    => true,
-		'em'      => true,
-		'strong'  => true,
-		'i'       => true,
-		'b'       => true
-	];
-
-	/**
-	 * Creates a new markup object. The constructor requires a `Breadcrumbs`
-	 * implementation and an optional array of arguments for configuring the
-	 * generated markup.
-	 */
-	public function __construct(
-		protected Breadcrumbs $breadcrumbs,
-		protected array $options
-	) {
-		$this->options = wp_parse_args($this->options, [
-			'show_on_front'      => false,
-			'show_first_item'    => true,
-			'show_last_item'     => true,
-			'before'             => '',
-			'after'              => '',
-			'container_tag'      => 'nav',
-			'title_tag'          => 'h2',
-			'list_tag'           => 'ol',
-			'item_tag'           => 'li',
-			'container_class'    => 'breadcrumbs',
-			'title_class'        => 'breadcrumbs__title',
-			'list_class'         => 'breadcrumbs__trail',
-			'item_class'         => 'breadcrumbs__crumb',
-			'item_content_class' => 'breadcrumbs__crumb-content',
-			'item_label_class'   => 'breadcrumbs__crumb-label'
-		]);
-
-		$this->options = apply_filters(
-			'x3p0/breadcrumbs/markup/config',
-			$this->options
-		);
-	}
-
-	/**
-	 * Helper method for grabbing the breadcrumbs array and removing any
-	 * items that should be removed.
-	 */
-	protected function crumbs(): array
-	{
-		$crumbs = $this->breadcrumbs->all();
-
-		// Remove the first crumb item if it's not supposed to be shown.
-		if (! $this->option('show_first_item')) {
-			array_shift($crumbs);
-		}
-
-		// Remove the last crumb item if it's not supposed to be shown.
-		if (! $this->option('show_last_item')) {
-			array_pop($crumbs);
-		}
-
-		return $crumbs;
-	}
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public function option(string $name): mixed
-	{
-		return isset($this->options[$name]) ? $this->options[$name] : null;
-	}
-
 	/**
 	 * {@inheritdoc}
 	 */

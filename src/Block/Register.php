ecb9664a1310a95e6f531f7cc162c55a9738658d
Justin Tadlock <justintadlock@gmail.com>
2025-09-21 10:54:48 -0500

    Remove back-compat block registration code.

--- src/Block/Register.php
+++ src/Block/Register.php
@@ -35,36 +35,12 @@ class Register implements Bootable
 	 * Registers the block with WordPress.
 	 *
 	 * @internal WordPress hook callback. Do not call directly.
-	 * @todo     Remove `function_exists()` with minimum WP 6.7 and 6.8 support.
 	 */
 	public function register(): void
 	{
-		// Bail if the manifest doesn't exist.
-		if (! file_exists("{$this->path}/manifest.php")) {
-			return;
-		}
-
-		// WordPress 6.8.
-		if (function_exists('wp_register_block_types_from_metadata_collection')) {
-			wp_register_block_types_from_metadata_collection(
-				$this->path,
-				"{$this->path}/manifest.php"
-			);
-			return;
-		}
-
-		// WordPress 6.7.
-		if (function_exists('wp_register_block_metadata_collection')) {
-			wp_register_block_metadata_collection(
-				$this->path,
-				"{$this->path}/manifest.php"
-			);
-		}
-
-		$manifest = include "{$this->path}/manifest.php";
-
-		foreach (array_keys($manifest) as $type) {
-			register_block_type("{$this->path}/{$type}");
-		}
+		wp_register_block_types_from_metadata_collection(
+			$this->path,
+			"{$this->path}/manifest.php"
+		);
 	}
 }

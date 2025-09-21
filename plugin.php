4462a5740c8e449f7dc85e7f371f293a4e966531
Justin Tadlock <justintadlock@gmail.com>
2025-09-21 10:54:08 -0500

    Bump WP minimum support to 6.8.

--- plugin.php
+++ plugin.php
@@ -4,8 +4,8 @@
  * Plugin Name:       X3P0: Breadcrumbs
  * Plugin URI:        https://github.com/x3p0-dev/x3p0-breadcrumbs
  * Description:       A breadcrumbs block for WordPress.
- * Version:           2.1.0
- * Requires at least: 6.6
+ * Version:           2.2.0-alpha
+ * Requires at least: 6.8
  * Requires PHP:      8.0
  * Author:            Justin Tadlock
  * Author URI:        https://justintadlock.com

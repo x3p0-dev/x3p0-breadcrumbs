c1c909b80ffbcab0d34e23779ee662742785c0f1
Justin Tadlock <justintadlock@gmail.com>
2025-11-07 12:04:53 -0600

    Add `.phpstorm.meta.php`.
    Just helping it understand the DI container.

---
+++ .phpstorm.meta.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * PhpStorm metadata, particularly for the service container.
+ *
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2009-2025, Justin Tadlock
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
+ * @link      https://github.com/x3p0-dev/x3p0-breadcrumbs
+ */
+
+declare (strict_types = 1);
+
+namespace PHPSTORM_META
+{
+	// For get() method.
+	override(\X3P0\Breadcrumbs\Core\Container::get(0), map(['' => '@']));
+
+	// For make() method.
+	override(\X3P0\Breadcrumbs\Core\Container::make(0), map(['' => '@']));
+}

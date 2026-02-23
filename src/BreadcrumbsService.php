4003401cf9dcd3eb16e42187fdb88e42e3c11dfa
Justin Tadlock <justintadlock@gmail.com>
2026-02-23 10:08:59 -0600

    Update copyright dates.

--- src/BreadcrumbsService.php
+++ src/BreadcrumbsService.php
@@ -4,7 +4,7 @@
  * Breadcrumbs service class.
  *
  * @author    Justin Tadlock <justintadlock@gmail.com>
- * @copyright Copyright (c) 2009-2025, Justin Tadlock
+ * @copyright Copyright (c) 2009-2026, Justin Tadlock
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
  * @link      https://github.com/x3p0-dev/x3p0-breadcrumbs
  */

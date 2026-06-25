7926a56e351c88af0d2851cf3084e52d83847d5f
Justin Tadlock <justintadlock@gmail.com>
2026-06-25 08:53:57 -0500

    Consistent file headers.

--- src/Assembler/Type/Paged.php
+++ src/Assembler/Type/Paged.php
@@ -4,9 +4,9 @@
  * Paged assembler.
  *
  * @author    Justin Tadlock <justintadlock@gmail.com>
- * @copyright Copyright (c) 2009-2025 Justin Tadlock
- * @link      https://github.com/x3p0-dev/x3p0-breadcrumbs
+ * @copyright Copyright (c) 2009-2026, Justin Tadlock
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
+ * @link      https://github.com/x3p0-dev/x3p0-breadcrumbs
  */
 
 declare(strict_types=1);
--- src/Crumb/Type/PostType.php
+++ src/Crumb/Type/PostType.php
@@ -5,9 +5,9 @@
  * title and its URL is the archive link for that post type.
  *
  * @author    Justin Tadlock <justintadlock@gmail.com>
- * @copyright Copyright (c) 2009-2025 Justin Tadlock
- * @link      https://github.com/x3p0-dev/x3p0-breadcrumbs
+ * @copyright Copyright (c) 2009-2026, Justin Tadlock
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
+ * @link      https://github.com/x3p0-dev/x3p0-breadcrumbs
  */
 
 declare(strict_types=1);

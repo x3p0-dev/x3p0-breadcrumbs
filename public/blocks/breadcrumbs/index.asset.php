26d38400721bd2047f75ad6faeb96c2197e02791
Justin Tadlock <justintadlock@gmail.com>
2026-06-28 19:56:47 -0500

    Stop tracking built public/ assets

--- public/blocks/breadcrumbs/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f406ba80743e1ffe8bc4');
